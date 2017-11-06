ad5d858a5e6dfb25a715a50fbc99d1444431c40a
pazhukh <pazhukh@gmail.com>
2017-11-06 12:49:58 +0200

    Update functions.php

--- functions.php
+++ functions.php
@@ -1,1 +1,14 @@
-
+//ajax оновлення кількості товару на сторінці кошика
+add_action( 'wp_footer', 'cart_update_qty_script' );
+function cart_update_qty_script() {
+  if (is_cart()) :
+   ?>
+    <script>
+        jQuery('div.woocommerce').on('change', '.qty', function(){
+           jQuery("[name='update_cart']").removeAttr('disabled');
+           jQuery("[name='update_cart']").trigger("click"); 
+        });
+   </script>
+<?php
+endif;
+}
